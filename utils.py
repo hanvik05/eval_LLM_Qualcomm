075cd6438253e03fa0c7c8fa9e8587b9f6d21838
hanvik05 <hanvik05@gmail.com>
2025-08-14 14:46:55 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 def add_numbers(numbers):
-    # BUG: forgot to use sum()
+    
     result = 0
     for num in numbers:
-        result =+ num  # wrong operator — should be +=
+        result =+ num  
     return result
